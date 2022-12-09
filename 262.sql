8f292a6708ff75ecaccbed049bcaef3aea156bbd
Scott Schmidt, MBA <scott.schmidt1989@yahoo.com>
2022-12-09 15:25:37 -0600

    Update 262.sql

--- 262.sql
+++ 262.sql
@@ -1,4 +1,17 @@
 #https://leetcode.com/problems/trips-and-users/
+#This method uses am IF statement which makes using a JOIN statement challenging;
+
+SELECT Request_at as Day,
+       ROUND(COUNT(IF(Status != 'completed', TRUE, NULL)) / COUNT(*), 2) AS 'Cancellation Rate'
+FROM Trips
+WHERE (Request_at BETWEEN '2013-10-01' AND '2013-10-03')
+AND client_id NOT IN (SELECT user_id FROM users WHERE banned= 'No');
+
+
+
+------------------------------------------------------------
+#Original solution reverses some days but everything's right
+
 SELECT t.request_at as Day, 
 round((COUNT(CASE when t.status <> "completed" THEN 1 ELSE NULL END)/
 COUNT(*)),2) as Cancellation_Rate
@@ -10,6 +23,7 @@ AND DATEDIFF(request_at, '2013-10-01')>=0
 AND DATEDIFF('2013-10-03 ', request_at)>=0
 GROUP BY request_at;
 
-# Currently solves everything except for the output days are reversed
+
+
 
 
