7a6692c94b2a55bdc977c77b7142cd11f73ca8ae
Scott Schmidt, MBA <scott.schmidt1989@yahoo.com>
2023-01-05 09:16:06 -0600

    Update scores.sql

--- scores.sql
+++ scores.sql
@@ -1,7 +1,8 @@
 #https://www.hackerrank.com/challenges/full-score/problem
 # My initial answer was incorrect because I JOINED difficulty first rather than challenges, Challenges must be first because it is joined later.
 # Must use count(*) DESC so it orders by number of times person scored full points
-# GROYO BY 1, 2 is shortcut for doing groupBy first two columns.
+# GROUPBY 1, 2 is shortcut for doing groupBy first two columns.
+# EDIT - GROUPBY 1,2 shouldn't be used as not very documentary and columns can change later
 
 SELECT h.hacker_id, h.name
 FROM submissions s
@@ -11,7 +12,7 @@ JOIN challenges c
 ON s.challenge_id=c.challenge_id
 JOIN difficulty d ON d.difficulty_level=c.difficulty_level
 AND s.score=d.score
-GROUP BY 1, 2
+GROUP BY h.hacker_id, h.name
 HAVING count(*) > 1
 ORDER BY count(*) DESC, h.hacker_id ASC
 ;
