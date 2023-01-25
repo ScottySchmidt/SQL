a752467e5dc694c24a4f22a13502c1186078a46b
Scott Schmidt, MBA <scott.schmidt1989@yahoo.com>
2023-01-25 12:38:53 -0600

    Update Wayfair_yoyGrowth.sql

--- Wayfair_yoyGrowth.sql
+++ Wayfair_yoyGrowth.sql
@@ -1,7 +1,7 @@
 Y-on-Y Growth Rate [Wayfair SQL Interview Question]
 https://datalemur.com/questions/yoy-growth-rate
-This is my first hard problem! And first time using LAG window_function.
-Need to reattempt the problem again had to use lots of research
+This is my first hard problem, first time LAG window_function.
+Need to reattempt the problem again had to use a hint
 
 ========================================
 with yearly_spend AS (
