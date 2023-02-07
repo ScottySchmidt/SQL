f7e925a93c4a584a8d4b045bb1a3d12656574034
Scott Schmidt, MBA <scott.schmidt1989@yahoo.com>
2023-02-07 12:35:06 -0600

    Update csv.sql

--- csv.sql
+++ csv.sql
@@ -34,6 +34,25 @@ WHERE total_sales-cogs<=0
 GROUP BY manufacturer
 )
 
+=================================================================
+Pharmacy Analytics (Part 3) [CVS Health SQL Interview Question]
+CVS Health is trying to better understand its pharmacy sales, and how well different products are selling. 
+Each drug can only be produced by one manufacturer.
+Write a query to find the total sales of drugs for each manufacturer.
+Round your answer to the closest million, and report your results in descending order of total sales.
+Because this data is being directly fed into a dashboard which is being seen by business stakeholders, format your result like this: "$36 million".
+--------------------------------------------------------
+
+with cvs as (SELECT manufacturer, sum(total_sales) as sales
+FROM pharmacy_sales
+GROUP BY manufacturer
+ORDER BY sales DESC
+)
+
+SELECT manufacturer, concat('$', round((sales/1000000),0) ,' million') as sales
+FROM cvs
+;
+
 SELECT manufacturer, drug_count,
 abs(net_total)
 FROM cvs
