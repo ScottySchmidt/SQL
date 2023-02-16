dc926b73b15895bb4aa016b431a8cb9f443f3205
Scott Schmidt, MBA <scott.schmidt1989@yahoo.com>
2023-02-16 13:24:47 -0600

    Update Hacker.sql

--- Hacker.sql
+++ Hacker.sql
@@ -1,18 +1,34 @@
 /*
 https://www.hackerrank.com/challenges/15-days-of-learning-sql/problem?isFullScreen=true
-ADVANCED - Hardest Hackerrank SQL problem?
+ADVANCED - Hardest Hackerrank SQL problem
 
 The start date of the contest was March 01, 2016 and the end date was March 15, 2016.
 Write a query to print total number of unique hackers who made at least  submission each day (starting on the first day of the contest). 
 Fnd the hacker_id and name of the hacker who made maximum number of submissions each day.
 If more than one such hacker has a maximum number of submissions, print the lowest hacker_id. 
 The query should print this information for each day of the contest, sorted by the date.
-
-This problem is the HARDEST hackerrank problem and is a work in process.
-I have portions of the problem done below.
 -----------------
 */
 
+#SETUP DAILY HACKERS TBL:
+DECLARE @daily_hackers TABLE (
+hacker_id INT,
+submission_date DATE
+);
+
+INSERT INTO @daily_hackers
+SELECT 
+hacker_id,
+submission_date
+FROM submissions 
+;
+
+SELECT hacker_id, submission_date
+FROM @daily_hackers;
+
+
+
+---------------------------------------
 --- person with most submissions each day:
 with max_sub as ( SELECT h.hacker_id, h.name, s.submission_date, count(h.hacker_id) as subs, 
 rank() OVER(PARTITION BY submission_date ORDER BY count(h.hacker_id) DESC, h.hacker_id ASC) as rn 
@@ -21,12 +37,11 @@ INNER JOIN submissions s
 ON h.hacker_id=s.hacker_id
 GROUP BY h.hacker_id, h.name, s.submission_date
                  )
----https://stackoverflow.com/questions/7395658/how-to-select-the-user-with-max-count-by-day
+                 
 SELECT submission_date, subs, hacker_id, name, rn
 FROM max_sub
 WHERE rn=1
 ;
-
 
 
 
