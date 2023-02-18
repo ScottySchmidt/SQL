63feabd2f8daba5ca0b19e5488e3d0c480b5aef2
Scott Schmidt, MBA <scott.schmidt1989@yahoo.com>
2023-02-18 13:37:19 -0600

    Create credit.sql

---
+++ credit.sql
@@ -0,0 +1,16 @@
+/*
+https://www.codewars.com/kata/5821ee33ec380124f1000013/train/sql
+Your company has an internal policy to determine your customers' credit limit, 
+but this procedure has been questioned recently by the board as being too conservative.
+
+Your CEO wants to increase the current customer base credit limits in order to upsell a new line of products. 
+In order to do that, the company hired several external consultancies to produce new credit limit estimates.
+
+The problem is that each agency has produced the report in its own format. 
+Some use the format "First-name Last-name" to identify a person, others use the format "Last-name, First-name". There is also no consensus on how to capitalize each word, so some used all uppercase, others used all lowercase, and some used mixed-case.
+
+Also, some names are titled, for example: "Dr. Hannibal Lecter", "Robert Downey Jr." etc. 
+you will need to pay attention to any such or similar cases.
+*/
+
+
