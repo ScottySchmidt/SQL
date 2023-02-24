a25e20b518667820077ea75a928da27947a52b62
Scott Schmidt, MBA <scott.schmidt1989@yahoo.com>
2023-02-24 13:44:38 -0600

    Update view.sql

--- view.sql
+++ view.sql
@@ -9,29 +9,29 @@ This is a great problem that has to join three datasets together and have two se
 1. JOIN the three datasets together.
 2. Use a subquery to find which departments have total spending above 10,000
 3. Use a groupby on the member id/name to find which member has spending above 1000.
-
-I plan to reattempt this problem later and make a YouTube video as I had to reverse engineer this problem as it was my first time using view instead of CTE.
+4. Make sure for the subquery to group by department.id (not regular id). The second time I solved this problem that was the only issue I had.
 ----------------------------------------
 */
 
-CREATE VIEW members_approved_for_voucher as 
-SELECT
-m.id, m.name, m.email, sum(p.price) as total_spending
+CREATE VIEW members_approved_for_voucher as
+SELECT m.id, m.name, m.email, sum(p.price) as total_spending
 FROM members m
-INNER JOIN sales s ON m.id=s.member_id
+INNER JOIN sales s ON s.member_id=m.id
 INNER JOIN products p ON p.id=s.product_id
+INNER JOIN departments d  ON d.id=s.department_id
 
-WHERE s.department_id IN (
+WHERE d.id IN (
 SELECT s2.department_id
-FROM sales s2
+FROM sales s2  
+INNER JOIN departments d2 on s2.department_id=d2.id  
 INNER JOIN products p2 ON p2.id=s2.product_id
 GROUP BY s2.department_id
 HAVING sum(p2.price)>10000
-  )
+)
 GROUP BY m.id, m.name, m.email
 HAVING sum(p.price)>1000
-ORDER BY m.id;
+ORDER BY m.id
+;
 
-SELECT id, name, email, total_spending
-FROM members_approved_for_voucher;
+
 
