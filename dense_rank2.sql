d83fb8ef2fdfc2157378b1a3e807c0d3dc4d333e
Scott Schmidt, MBA <scott.schmidt1989@yahoo.com>
2023-02-25 11:35:30 -0600

    Delete dense_rank2.sql

--- dense_rank2.sql
+++
@@ -1,8 +0,0 @@
-# Only a Medium level problem but good DENSE_RANK practice
-#https://leetcode.com/problems/rank-scores/description/
-SELECT s.score, DENSE_RANK () OVER (
-    ORDER BY s.score DESC 
-) as 'rank'
-FROM scores s;
-
-#Sources: https://www.sqlservertutorial.net/sql-server-window-functions/sql-server-dense_rank-function/
