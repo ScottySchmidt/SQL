bcd6363ef47f34a9f75b2a230443516264d989fe
Scott Schmidt, MBA <scott.schmidt1989@yahoo.com>
2023-02-27 14:40:38 -0600

    Update Twtter2.sql

--- Twtter2.sql
+++ Twtter2.sql
@@ -10,14 +10,19 @@ Instruction
 Write a query to find the activation day for every account.
 Return null if an account has not activated any of the campaigns yet.
 
-In progress..
+This solution is solved easily by using a row_number window function which can put a row_num on each account_id by date.
 */
 
-
-SELECT c.account_id, min(s.date) as start_date
+with twitter as(
+SELECT c.account_id, s.date,
+row_number() OVER(PARTITION BY account_id ORDER BY s.date ASC) as rn
 FROM twitter_campaigns c
 JOIN twitter_campaign_spend s
 ON c.campaign_id = s.campaign_id
 GROUP BY c.account_id, s.date
 ORDER BY c.account_id
+)
 
+select account_id, date
+FROM twitter
+WHERE rn=1
