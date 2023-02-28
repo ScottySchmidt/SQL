62142b6be84c3f8f1926c678e7e1db6d1fea556d
Scott Schmidt, MBA <scott.schmidt1989@yahoo.com>
2023-02-28 10:18:19 -0600

    Update six_tables.sql

--- six_tables.sql
+++ six_tables.sql
@@ -1,7 +1,10 @@
 /*
 https://sqlpad.io/questions/49/top-3-money-making-movie-categories/
 Top 3 money making movie categories
+
 This problem requires joining six large tables together which is a large amount.
+The difficult part becomes knowing when to use LEFT or INNER JOIN.
+Using a SUM(P.AMOUNT) order by at the end gets a memory error.
 */
 
 SELECT c.name, c.category_id, fc.film_id, i.store_id, p.amount
