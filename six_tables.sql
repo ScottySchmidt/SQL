9e2d58948433e7e4322dfeeaa7bc5690d9bdacf9
Scott Schmidt, MBA <scott.schmidt1989@yahoo.com>
2023-02-28 11:15:50 -0600

    Update six_tables.sql

--- six_tables.sql
+++ six_tables.sql
@@ -8,7 +8,22 @@ Using a simple INNER JOIN on the primary key IDS should match all the tables cor
 Using a LIMIT 3 at the end creates a memory error more research needed.
 Therefore, a window function row_number is one solution to make the solution faster:
 https://community.snowflake.com/s/article/Out-of-memory-error-caused-by-LIMIT-and-or-OFFSET-clause
+Using a row_number also creates a memory error but this is the correct solution as well.
 */
+
+SELECT c.name,  
+row_number() OVER (PARTITION BY c.name ORDER BY p.amount DESC ) as rn
+FROM category c
+INNER JOIN film_category fc
+ON c.category_id = fc.category_id
+LEFT JOIN inventory i
+ON fc.film_id=i.film_id
+INNER JOIN rental r
+ON r.inventory_id=i.inventory_id
+INNER JOIN payment p
+ON p.customer_id=r.customer_id
+
+
 
 SELECT c.name,  sum(p.amount)
 FROM category c
@@ -22,3 +37,6 @@ INNER JOIN payment p
 ON p.customer_id=r.customer_id
 GROUP BY c.name
 ORDER BY sum(p.amount) DESC
+
+
+
