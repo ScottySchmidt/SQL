9af3a173ab1371a3d169914979a3c62ffaaa818b
Scott Schmidt, MBA <scott.schmidt1989@yahoo.com>
2023-02-28 10:49:22 -0600

    Update six_tables.sql

--- six_tables.sql
+++ six_tables.sql
@@ -4,16 +4,18 @@ Top 3 money making movie categories
 
 This problem requires joining six large tables together which is a large amount.
 The difficult part becomes knowing when to use LEFT or INNER JOIN.
-Using a SUM(P.AMOUNT) order by at the end gets a memory error.
+Using a LIMIT 3 at the end creates a memory error more research needed.
 */
 
-SELECT c.name, c.category_id, fc.film_id, i.store_id, p.amount
+SELECT c.name,  sum(p.amount)
 FROM category c
-LEFT JOIN film_category fc
+INNER JOIN film_category fc
 ON c.category_id = fc.category_id
 LEFT JOIN inventory i
 ON fc.film_id=i.film_id
-LEFT JOIN rental r
+INNER JOIN rental r
 ON r.inventory_id=i.inventory_id
-LEFT JOIN payment p
+INNER JOIN payment p
 ON p.customer_id=r.customer_id
+GROUP BY c.name
+ORDER BY sum(p.amount) DESC
