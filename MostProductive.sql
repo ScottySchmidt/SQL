7e2e33be1e6a454c7ad8657064d87fe1e26b2760
Scott Schmidt, MBA <scott.schmidt1989@yahoo.com>
2023-03-02 23:23:51 -0600

    Update MostProductive.sql

--- MostProductive.sql
+++ MostProductive.sql
@@ -3,14 +3,24 @@ HARD SQLpad problem
 The most productive actors by category 
 https://sqlpad.io/questions/74/the-most-productive-actors-by-category/
 
-in process..
+Using max in (fa.film_id) is just a way to use an agg within a groupnby later to not throw an error.
 */
 
-
-SELECT cat.category_id, fa.actor_id, count(fa.film_id) as movie_num
+with categories as (SELECT cat.category_id, 
+max(fa.actor_id) as actor_id,
+count(fa.film_id) as movie_num,
+rank() over(PARTITION BY cat.category_id ORDER BY count(fa.film_id) DESC) as rn
 FROM film_actor fa
 INNER JOIN film_category cat
 ON fa.film_id=cat.film_id
-GROUP BY fa.actor_id, cat.category_id
-ORDER BY cat.category_id, count(fa.film_id) DESC
+GROUP BY cat.category_id, fa.actor_id
+ORDER BY cat.category_id
+)
+
+SELECT category_id, actor_id, movie_num
+FROM categories
+WHERE rn=1
+ORDER BY category_id
+
+
 
