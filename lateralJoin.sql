407653b7e8fbb8746ccf64c1eb4ccea54c68adb3
Scott Schmidt, MBA <scott.schmidt1989@yahoo.com>
2023-03-06 21:54:46 -0600

    Update lateralJoin.sql

--- lateralJoin.sql
+++ lateralJoin.sql
@@ -2,6 +2,18 @@
 Using LATERAL JOIN To Get Top N per Group
 https://www.codewars.com/kata/5820176255c3d23f360000a9/train/sql
 
+Given the schema presented below write a query, which uses a window function, that returns two most viewed posts for every category.
+
+Order the result set by:
+
+category name alphabetically
+number of post views largest to lowest
+post id lowest to largest
+Note:
+
+Some categories may have less than two or no posts at all.
+Two or more posts within the category can be tied by (have the same) the number of views. Use post id as a tie breaker - a post with a lower id gets a higher rank.
+
 Same problem two times. 
 The firt time solve the problem using a window function.
 The second time use a left Lateral join.
