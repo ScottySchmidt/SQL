6c77584dde54810c6d313b63917681103ce3703f
Scott Schmidt, MBA <scott.schmidt1989@yahoo.com>
2023-03-09 11:43:04 -0600

    Update and rename 262.sql to cancelRate262.sql

--- cancelRate262.sql
+++ cancelRate262.sql
@@ -1,5 +1,10 @@
-#https://leetcode.com/problems/trips-and-users/
-#This method uses am IF statement which makes using a JOIN statement challenging;
+/* 
+HARD Leetcode SQL https://leetcode.com/problems/trips-and-users/
+The cancellation rate is computed by dividing the number of canceled (by client or driver) requests with unbanned users by the total number of requests with unbanned users on that day.
+Write a SQL query to find the cancellation rate of requests with unbanned users (both client and driver must not be banned) each day between "2013-10-01" and "2013-10-03".
+Round Cancellation Rate to two decimal points.
+Return the result table in any order.
+*/
 
 SELECT Request_at as Day,
        ROUND(COUNT(IF(Status != 'completed', TRUE, NULL)) / COUNT(*), 2) AS 'Cancellation Rate'
@@ -9,7 +14,7 @@ AND client_id NOT IN (SELECT user_id FROM users WHERE banned= 'No');
 
 
 
-------------------------------------------------------------
+
 #Original solution reverses some days but everything's right
 
 SELECT t.request_at as Day, 
