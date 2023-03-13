680dcddebb4db92998c1cb4d65a57c5832163f97
Scott Schmidt, MBA <scott.schmidt1989@yahoo.com>
2023-03-13 14:22:37 -0500

    Update Stripe2.sql

--- Stripe2.sql
+++ Stripe2.sql
@@ -4,15 +4,15 @@ Assume you are given the table below containing information on user transactions
 Write a query to obtain all of the users' rolling 3-day earnings. Output the user ID, transaction date and rolling 3-day earnings.
 */
 
-with stripe as(SELECT user_id, round(sum(amount),2) as total, 
-cast(transaction_date as date)
+# This was my first solution which correctly gets the rolling 3 day sum by rows:
+with earnings as (SELECT user_id, sum(amount) as earnings, cast(transaction_date as date)
 FROM user_transactions
 GROUP BY user_id, cast(transaction_date as date)
 )
 
-SELECT user_id, transaction_date,
-avg(total) OVER(ORDER BY transaction_date
-     ROWS BETWEEN 2 PRECEDING AND CURRENT ROW )
-     as rolling_earnings_3d
-     FROM stripe
-     ORDER BY user_id, transaction_date
+SELECT user_id, transaction_date, 
+sum(earnings) OVER(PARTITION BY user_id ORDER BY transaction_date ROWS BETWEEN 2 PRECEDING AND CURRENT ROW) as rolling_earnings_3d
+FROM earnings
+;
+
+#However, the above method should only sum if the days are 3 days previously. 
