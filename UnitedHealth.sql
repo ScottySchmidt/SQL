87fe28fff343d9964c06b588ce9bff2c9a6f2266
Scott Schmidt, MBA <scott.schmidt1989@yahoo.com>
2023-03-15 14:48:42 -0500

    Update UnitedHealth.sql

--- UnitedHealth.sql
+++ UnitedHealth.sql
@@ -8,13 +8,23 @@ which allows members to call an advocate and receive support for their health ca
 Write a query to get the patients who made a call within 7 days of their previous call. 
 If a patient called more than twice in a span of 7 days, count them as once.
 
-Use of Lag likely required which is what I assumed:
-https://stackoverflow.com/questions/52874919/returning-data-where-dates-are-at-least-7-days-apart
-
-After research, I found EPOCH which can convert datetime into seconds.
-This code needs cleaned but gets the correct solution.
+Use of Lag: https://stackoverflow.com/questions/52874919/returning-data-where-dates-are-at-least-7-days-apart
+Note: PostSQL uses date_part and not the typical datediff.
 */
 
+with cte as (SELECT policy_holder_id, 
+call_received, 
+lag(call_received, 1) OVER(PARTITION BY policy_holder_id ORDER BY date(call_received)) as prev_date
+FROM callers
+)
+
+SELECT count(distinct(policy_holder_id))
+FROM cte
+where date_part('day', call_received-prev_date)<7;
+
+
+
+---After research, I found EPOCH which can convert datetime into seconds.
 with united_policy as (
 SELECT policy_holder_id,
 call_received,
