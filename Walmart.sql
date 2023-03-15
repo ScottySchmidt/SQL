7b75b1cd2b6a36778091f898bd8a8164037cd565
Scott Schmidt, MBA <scott.schmidt1989@yahoo.com>
2023-03-14 20:13:31 -0500

    Update Walmart.sql

--- Walmart.sql
+++ Walmart.sql
@@ -1,10 +1,11 @@
-# https://datalemur.com/questions/frequently-purchased-pairs
-# Frequently Purchased Pairs [Walmart SQL Interview Question]
+/* Frequently Purchased Pairs [Walmart SQL Interview Question]  https://datalemur.com/questions/frequently-purchased-pairs
+
 Assume you are given the following tables on Walmart transactions and products. 
 Find the number of unique product combinations that are bought together (purchased in the same transaction).
 For example, if I find two transactions where apples and bananas are bought, and another transaction where bananas and soy milk are bought,
 my output would be 2 to represent the 2 unique combinations. 
 -------------------------------------------------------------
+*/
 
 with purchase as (
 SELECT t.transaction_id, t.product_id
