119be4f79cfe75d13b7ca6b9c58e4142f79c57df
Scott Schmidt, MBA <scott.schmidt1989@yahoo.com>
2023-03-21 14:36:07 -0500

    Update JPM.sql

--- JPM.sql
+++ JPM.sql
@@ -1,10 +1,12 @@
-# https://datalemur.com/questions/card-launch-success
-# Card Launch Success [JPMorgan Chase SQL Interview Question]
+/*  
+Card Launch Success [JPMorgan Chase SQL Interview Question] https://datalemur.com/questions/card-launch-success
 Your team at JPMorgan Chase is soon launching a new credit card. 
 You are asked to estimate how many cards you'll issue in the first month.
 Before you can answer this question, you want to first get some perspective on how well new credit card launches typically do in their first month.
 Write a query that outputs the name of the credit card, and how many cards were issued in its launch month. 
 The launch month is the earliest record in the monthly_cards_issued table for a given card. Order the results starting from the biggest issued amount.
+--------------------------------------
+*/
 
 with cards AS (
 SELECT card_name, issued_amount, issue_month, issue_year,
