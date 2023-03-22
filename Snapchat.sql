7b64d369dbd5f5f4c525c685d359b3b6f47b4978
Scott Schmidt, MBA <scott.schmidt1989@yahoo.com>
2023-03-22 11:16:18 -0500

    Update Snapchat.sql

--- Snapchat.sql
+++ Snapchat.sql
@@ -1,5 +1,5 @@
-# https://datalemur.com/questions/time-spent-snaps
-# Sending vs. Opening Snaps [Snapchat SQL Interview Question]
+/* 
+Sending vs. Opening Snaps [Snapchat SQL Interview Question] https://datalemur.com/questions/time-spent-snaps
 Assume you are given the tables below containing information on Snapchat users, their ages, and their time spent sending and opening snaps.
 Write a query to obtain a breakdown of the time spent sending vs. opening snaps (as a percentage of total time spent on these activities) for each age group.
 Output the age bucket and percentage of sending and opening snaps. Round the percentage to 2 decimal places.
@@ -8,8 +8,8 @@ You should calculate these percentages:
 time sending / (time sending + time opening)
 time opening / (time sending + time opening)
 To avoid integer division in percentages, multiply by 100.0 and not 100.
-
 ------------------------------------
+*/
 
 WITH snap AS (
 SELECT 
