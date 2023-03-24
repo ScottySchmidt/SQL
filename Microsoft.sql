1b78b337230d63e98505c8172b7985f16645a0da
Scott Schmidt, MBA <scott.schmidt1989@yahoo.com>
2023-03-24 16:14:55 -0500

    Update Microsoft.sql

--- Microsoft.sql
+++ Microsoft.sql
@@ -1,7 +1,15 @@
-# https://datalemur.com/questions/supercloud-customer
-# Microsoft SQL Interview Question
+/* 
+Microsoft SQL Interview Question:  https://datalemur.com/questions/supercloud-customer
 
-# Use t1 as a temp table to get the unique count of products using customer_id
+A Microsoft Azure Supercloud customer is a company which buys at least 1 product from each product category.
+Write a query to report the company ID which is a Supercloud customer.
+As of 5 Dec 2022, data in the customer_contracts and products tables were updated.
+
+Customer 1 bought from Analytics, Containers, and Compute categories of Azure, and thus is a Supercloud customer. 
+Customer 2 isn't a Supercloud customer, since they don't buy any container services from Azure.
+*/ 
+
+---Use t1 as a temp table to get the unique count of products using customer_id
 with t1 as(
 SELECT c.customer_id,
 count(DISTINCT p.product_category) as unique_count
