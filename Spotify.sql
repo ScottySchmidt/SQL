b68a53b141c7be07fb92f90bbe179d4462ed44cb
Scott Schmidt, MBA <scott.schmidt1989@yahoo.com>
2023-03-25 11:00:12 -0500

    Update Spotify.sql

--- Spotify.sql
+++ Spotify.sql
@@ -1,9 +1,20 @@
 /*
-Spotify HARD SQL https://datalemur.com/questions/top-fans-rank
-Original solution need to make code more clean
-DENSE RANK does not require a PARTITION 
+Spotify SQL https://datalemur.com/questions/top-fans-rank
+
+Assume there are three Spotify tables containing information about the artists, songs, and music charts. Write a query to determine the top 5 artists whose songs appear in the Top 10 of the global_song_rank table the highest number of times. From now on, we'll refer to this ranking number as "song appearances".
+Output the top 5 artist names in ascending order along with their song appearances ranking (not the number of song appearances, but the rank of who has the most appearances). The order of the rank should take precedence.
+For example, Ed Sheeran's songs appeared 5 times in Top 10 list of the global song rank table; this is the highest number of appearances, so he is ranked 1st. Bad Bunny's songs appeared in the list 4, so he comes in at a close 2nd.
+
+Assumptions:
+If two artists' songs have the same number of appearances, the artists should have the same rank.
+The rank number should be continuous (1, 2, 2, 3, 4, 5) and not skipped (1, 2, 2, 4, 5).
+
+My Notes:  
+1. DENSE RANK does not require a PARTITION 
 */
 
+
+--Original solution need to make code more clean
 with t1 AS (
 SELECT 
   songs.artist_id,
