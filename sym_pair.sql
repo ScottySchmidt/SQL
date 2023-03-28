5ae470cea9788c697e148b6a2aa82625cf008ffc
Scott Schmidt, MBA <scott.schmidt1989@yahoo.com>
2023-03-28 11:32:23 -0500

    Update sym_pair.sql

--- sym_pair.sql
+++ sym_pair.sql
@@ -1,14 +1,11 @@
-/* https://www.hackerrank.com/challenges/symmetric-pairs/problem?isFullScreen=true
+/*
+Symmetric Pairs:  https://www.hackerrank.com/challenges/symmetric-pairs/problem?isFullScreen=true
 Two pairs (X1, Y1) and (X2, Y2) are said to be symmetric pairs if X1 = Y2 and X2 = Y1.
 Write a query to output all such symmetric pairs in ascending order by the value of X. List the rows such that X1 ≤ Y1.
-
-This problem took me some research to understand.
 Specifically, the line: HAVING COUNT(*)>1 OR a.X!=a.Y
-
 This above code will find a pair that is a duplicate. 
 But then distinct will only count this pair once.
 The or statement is needed because typically a sym pair are not the same x is less than y.
-If I had more time I would be to a better understanding of sym pairs this problem definitely makes one think.
 */ 
 
 ---------------
@@ -20,4 +17,4 @@ GROUP BY a.X, a.Y
 HAVING COUNT(*)>1 OR a.X!=a.Y
 ORDER BY a.X
 ;
-;
+
