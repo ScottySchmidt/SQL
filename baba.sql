29332b022b1c47befc119bef873bee47169e2c25
Scott Schmidt, MBA <scott.schmidt1989@yahoo.com>
2023-03-28 16:17:10 -0500

    Update baba.sql

--- baba.sql
+++ baba.sql
@@ -1,14 +1,14 @@
 /*
-Compressed Mode [Alibaba SQL Interview Question]
-https://datalemur.com/questions/alibaba-compressed-mode
+Compressed Mode [Alibaba SQL Interview Question] https://datalemur.com/questions/alibaba-compressed-mode
 
-Source:
-https://stackoverflow.com/questions/12235595/find-most-frequent-value-in-sql-column
-
-This problem gets the correct answer.
-However, using limit1 is not prefered as mode could require an average of two top.
+You are trying to find the most common (aka the mode) number of items bought per order on Alibaba.
+However, instead of doing analytics on all Alibaba orders, you have access to a summary table, which describes how many items were in an order (item_count), 
+and the number of orders that had that many items (order_occurrences).
+In case of multiple item counts, display the item_counts in ascending order.
 */
 
+
+--- This problem gets the correct answer.However, using limit1 is not prefered as mode could require an average of two top.
 with baba as (SELECT order_occurrences,
 count(order_occurrences) as value_counts
 FROM items_per_order
@@ -31,6 +31,7 @@ WHERE order_occurrences=(SELECT order_occurrences FROM baba)
 SELECT item_count as mode 
 FROM occur
 ;
+---Source: https://stackoverflow.com/questions/12235595/find-most-frequent-value-in-sql-column
 
 ---SELECT item_count, order_occurrences
 ---FROM items_per_order
