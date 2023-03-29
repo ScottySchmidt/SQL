db405af9992437775169b296474dc58bae541b1e
Scott Schmidt, MBA <scott.schmidt1989@yahoo.com>
2023-03-29 14:48:53 -0500

    Update Uber.sql

--- Uber.sql
+++ Uber.sql
@@ -1,10 +1,17 @@
-# https://datalemur.com/questions/sql-third-transaction
-# User's Third Transaction [Uber SQL Interview Question]
+/* 
+User's Third Transaction [Uber SQL Interview Question]  https://datalemur.com/questions/sql-third-transaction
 Assume you are given the table below on Uber transactions made by users.
 Write a query to obtain the third transaction of every user. Output the user id, spend and transaction date.
 
-This problem was rather simple because it was similiar to the JP Morgan problem
-
+transactions Example Input:
+user_id	spend	transaction_date
+111	100.50	01/08/2022 12:00:00
+111	55.00	01/10/2022 12:00:00
+121	36.00	01/18/2022 12:00:00
+145	24.99	01/26/2022 12:00:00
+111	89.60	02/05/2022 12:00:00
+--------------------------------------------------
+*/ 
 WITH uber AS (
 SELECT user_id, spend, transaction_date,
 rank() OVER (
