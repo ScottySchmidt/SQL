9fae0a79a57fac260b0fc6f440df2233b6356ea9
Scott Schmidt, MBA <scott.schmidt1989@yahoo.com>
2023-04-18 19:45:08 -0500

    Update amazon1321.sql

--- amazon1321.sql
+++ amazon1321.sql
@@ -17,7 +17,25 @@ amount is the total paid by a customer.
 -----------------------------------
 */
 
-# Initial start that has everything correct but last row:
+-- Final solution beats 82%:
+with day_totals as (
+SELECT visited_on, sum(amount) as amount
+FROM customer
+GROUP BY visited_on
+)
+
+SELECT visited_on, 
+round(sum(amount) OVER(ORDER BY visited_on ROWS BETWEEN 6 PRECEDING AND CURRENT ROW), 2) as amount,
+round(avg(amount) OVER(ORDER BY visited_on ROWS BETWEEN 6 PRECEDING AND CURRENT ROW), 2) as average_amount
+FROM day_totals
+GROUP BY visited_on 
+ORDER BY visited_on
+LIMIT 99999999999 OFFSET 6
+
+
+
+-- The above solution fixed the bottom solution because you first have to do a groupby on the days:
+-- Initial start that has everything correct but last row:
 SELECT visited_on, 
 round(sum(amount) OVER(ORDER BY visited_on ROWS BETWEEN 6 PRECEDING and current row), 2) as amount,
 round(avg(amount) OVER(ORDER BY visited_on ROWS BETWEEN 6 PRECEDING and current row), 2) as average_amount
