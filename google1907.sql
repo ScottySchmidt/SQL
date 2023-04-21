b72133135c1b50594037bd09ea8d19f249648a06
Scott Schmidt, MBA <scott.schmidt1989@yahoo.com>
2023-04-21 08:57:55 -0500

    Create google1907.sql

---
+++ google1907.sql
@@ -0,0 +1,33 @@
+/*
+1907. Count Salary Categories, Medium level: https://leetcode.com/problems/count-salary-categories/
+
+Table: Accounts
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| account_id  | int  |
+| income      | int  |
++-------------+------+
+account_id is the primary key for this table.
+Each row contains information about the monthly income for one bank account.
+ 
+Write an SQL query to report the number of bank accounts of each salary category. The salary categories are:
+"Low Salary": All the salaries strictly less than $20000.
+"Average Salary": All the salaries in the inclusive range [$20000, $50000].
+"High Salary": All the salaries strictly greater than $50000.
+The result table must contain all three categories. If there are no accounts in a category, then report 0.
+*/
+
+-- Passes 10/11, does not find a salary if null need to fix this bug:
+with cte as (SELECT account_id, 
+CASE
+when income < 20000 then 'Low Salary' 
+when income <= 50000 and income >=20000 then 'Average Salary'
+when income > 50000 then 'High Salary'
+END as category
+FROM Accounts
+) 
+
+SELECT category, count(account_id) as accounts_count
+FROM cte
+GROUP BY category
