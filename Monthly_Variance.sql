48d5d869f814f4ce721ad4c84418ed1a7ef58ddf
Scott Schmidt, MBA <scott.schmidt1989@yahoo.com>
2023-09-21 19:13:19 -0500

    Create Monthly_Variance.sql

---
+++ Monthly_Variance.sql
@@ -0,0 +1,17 @@
+/*
+Monthly Percentage Difference Amazon: https://platform.stratascratch.com/coding/10319-monthly-percentage-difference?code_type=5
+
+Given a table of purchases by date, calculate the month-over-month percentage change in revenue. The output should include the year-month date (YYYY-MM) and percentage change, rounded to the 2nd decimal point, and sorted from the beginning of the year to the end of the year.
+The percentage change column will be populated from the 2nd month forward and can be calculated as ((this month's revenue - last month's revenue) / last month's revenue)*100.
+*/ 
+
+with amazon_sales as (SELECT id, month(created_at) as mth, year(created_at) as year, value, purchase_id 
+FROM sf_transactions),
+
+monthly_amazon_sales as (SELECT id, sum(value) as monthly_sales, min(mth) as mth, min(year) as year
+FROM amazon_sales
+GROUP BY id, mth, year)
+
+SELECT id, monthly_sales, mth, year, lag(monthly_sales) OVER(PARTITION BY id ORDER BY id) as prev_month
+FROM monthly_amazon_sales
+;
