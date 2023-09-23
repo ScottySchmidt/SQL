d1bd8a33ef1505e4a5c2613b541dd11e359a4410
Scott Schmidt, MBA <scott.schmidt1989@yahoo.com>
2023-09-23 13:27:53 -0500

    Update MonthVariance.sql

--- MonthVariance.sql
+++ MonthVariance.sql
@@ -1,7 +1,8 @@
 /*
 Monthly Percentage Difference Amazon: https://platform.stratascratch.com/coding/10319-monthly-percentage-difference?code_type=5
 
-Given a table of purchases by date, calculate the month-over-month percentage change in revenue. The output should include the year-month date (YYYY-MM) and percentage change, rounded to the 2nd decimal point, and sorted from the beginning of the year to the end of the year.
+Given a table of purchases by date, calculate the month-over-month percentage change in revenue. 
+The output should include the year-month date (YYYY-MM) and percentage change, rounded to the 2nd decimal point, and sorted from the beginning of the year to the end of the year.
 The percentage change column will be populated from the 2nd month forward and can be calculated as ((this month's revenue - last month's revenue) / last month's revenue)*100.
 */ 
 
