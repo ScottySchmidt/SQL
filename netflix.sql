1ba329dd05cabadf5b48d9ee915c993badf956fe
Scott Schmidt, MBA <scott.schmidt1989@yahoo.com>
2023-11-03 10:06:43 -0500

    Update netflix.sql

--- netflix.sql
+++ netflix.sql
@@ -5,7 +5,7 @@ Find the genre of the person with the most number of oscar winnings.
 If there are more than one person with the same number of oscar wins, return the first one in alphabetic order based on their name. Use the names as keys when joining the tables.
 */
 
---SQL Server:
+--SQL Server and mySQL:
 with netflix as (SELECT oscar.category as genre, 
 oscar.nominee as full_name, 
 count(oscar.id) as number_oscars
