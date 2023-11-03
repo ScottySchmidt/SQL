d97a0f2d725ca42cb6441259e68730471860dbea
Scott Schmidt, MBA <scott.schmidt1989@yahoo.com>
2023-11-03 10:02:54 -0500

    Create netflix.sql

---
+++ netflix.sql
@@ -0,0 +1,22 @@
+/*
+Find the genre of the person with the most number of oscar winnings: https://platform.stratascratch.com/coding/10171-find-the-genre-of-the-person-with-the-most-number-of-oscar-winnings?code_type=5
+
+Find the genre of the person with the most number of oscar winnings.
+If there are more than one person with the same number of oscar wins, return the first one in alphabetic order based on their name. Use the names as keys when joining the tables.
+*/
+
+--SQL Server:
+with netflix as (SELECT oscar.category as genre, 
+oscar.nominee as full_name, 
+count(oscar.id) as number_oscars
+FROM oscar_nominees AS oscar
+LEFT JOIN nominee_information AS person 
+ON oscar.nominee = person.name
+WHERE winner = 'TRUE' 
+GROUP BY oscar.category, oscar.nominee
+)
+
+SELECT full_name, genre, number_oscars
+FROM netflix
+ORDER BY number_oscars DESC, full_name
+;
