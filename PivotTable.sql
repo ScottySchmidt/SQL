8e6d68166dcc47d7328b3e09c44fa2c5f05e3d6c
Scott Schmidt, MBA <scott.schmidt1989@yahoo.com>
2023-11-05 10:45:45 -0600

    Create PivotTable.sql

---
+++ PivotTable.sql
@@ -0,0 +1,12 @@
+/*  
+City of San Fran: https://platform.stratascratch.com/coding/10145-make-a-pivot-table-to-find-the-highest-payment-in-each-year-for-each-employee?code_type=5
+Make a pivot table to find the highest payment in each year for each employee
+Make a pivot table to find the highest payment in each year for each employee.
+Find payment details for 2011, 2012, 2013, and 2014.
+Output payment details along with the corresponding employee name.
+Order records by the employee name in ascending order
+*/
+
+SELECT id, employeename
+FROM sf_public_salaries
+WHERE YEAR in (2011, 2012, 2013, 2014);
