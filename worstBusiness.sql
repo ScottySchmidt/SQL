8a0ba5d1f96b33a4c188306f5247a8490f331342
Scott Schmidt, MBA <scott.schmidt1989@yahoo.com>
2023-11-09 14:06:14 -0600

    Create worstBusiness.sql

---
+++ worstBusiness.sql
@@ -0,0 +1,17 @@
+/* Worst Businesses, Hard SQL: https://platform.stratascratch.com/coding/9739-worst-businesses?code_type=5
+For every year, find the worst business in the dataset. The worst business has the most violations during the year. You should output the year, business name, and number of violations.
+*/ 
+
+--SQL Server:
+with business as (SELECT business_name, year(inspection_date) as inspect_year, COUNT(business_name) as violations_count
+FROM sf_restaurant_health_violations
+GROUP BY business_name, year(inspection_date)
+), 
+
+business2 as (SELECT business_name, inspect_year, violations_count,
+ROW_NUMBER() OVER (PARTITION BY inspect_year ORDER BY violations_count DESC) as rn
+FROM business)
+
+SELECT business_name, inspect_year, violations_count
+FROM business2
+WHERE rn = 1
