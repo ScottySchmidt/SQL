c0bc1a30fb0ac58aadf5e10a85b04c2f406a1df1
Scott Schmidt, MBA <scott.schmidt1989@yahoo.com>
2023-11-09 16:00:55 -0600

    Update worstBusiness.sql

--- worstBusiness.sql
+++ worstBusiness.sql
@@ -15,3 +15,22 @@ FROM business)
 SELECT business_name, inspect_year, violations_count
 FROM business2
 WHERE rn = 1
+
+
+--- Python
+import pandas as pd
+
+# Assuming df is your DataFrame with 'business_name' and 'inspection_date'
+df = sf_restaurant_health_violations[['business_name', 'inspection_date']]
+
+# Add a 'Year' column
+df['Year'] = df['inspection_date'].dt.year
+
+# Group by 'Year' and 'business_name', find the maximum count for each combination
+count_df = df.groupby(['Year', 'business_name']).size().reset_index(name='count')
+
+# Find the index of the maximum count for each year
+max_counts_index = count_df.groupby('Year')['count'].idxmax()
+
+# Get the corresponding rows with the highest count for each year
+max_counts = count_df.loc[max_counts_index]
