ea9f0255cf72d29a8a07c03ca2ebc23ecae9e21b
Scott Schmidt, MBA <scott.schmidt1989@yahoo.com>
2023-11-17 13:01:54 -0600

    Update and rename Lyft.sql to CancelRate.sql

--- CancelRate.sql
+++ CancelRate.sql
@@ -1,5 +1,5 @@
 /* 
-HARD Leetcode SQL https://leetcode.com/problems/trips-and-users/
+Trips and Users Cancel Rate SQL: https://leetcode.com/problems/trips-and-users/
 The cancellation rate is computed by dividing the number of canceled (by client or driver) requests with unbanned users by the total number of requests with unbanned users on that day.
 Write a SQL query to find the cancellation rate of requests with unbanned users (both client and driver must not be banned) each day between "2013-10-01" and "2013-10-03".
 Round Cancellation Rate to two decimal points.
