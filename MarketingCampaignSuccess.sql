bd9f8590c649ba3d6d01b33857dc164dfacd275b
Scott Schmidt, MBA <scott.schmidt1989@yahoo.com>
2023-11-20 15:37:24 -0600

    Update MarketingCampaignSuccess.sql

--- MarketingCampaignSuccess.sql
+++ MarketingCampaignSuccess.sql
@@ -5,6 +5,18 @@ Find the number of users that made additional in-app purchases due to the succes
 The marketing campaign doesn't start until one day after the initial in-app purchase so users that only made one or multiple purchases on the first day do not count, nor do we count users that over time purchase only the products they purchased on the first day.
 */
 
+---SQL Server and MySQL
+with cte as (SELECT user_id, created_at, product_id,
+rank() over(partition by user_id order by created_at) date_rank,
+RANK() OVER (PARTITION BY product_id, user_id ORDER BY created_at) AS prod_rank
+FROM marketing_campaign
+)
+
+SELECT count(DISTINCT user_id) FROM cte
+WHERE date_rank > 1
+AND prod_rank =1
+;
+
 
 --SQL Server, this solution is incorrect by 5% because you must find the number of same products too:
 with cte as (SELECT user_id, created_at,
