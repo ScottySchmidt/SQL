b34b6633332e4b634feb49219e68126e2dfae5df
Scott Schmidt, MBA <scott.schmidt1989@yahoo.com>
2023-11-24 11:56:46 -0600

    Update TopPercentileFraud.sql

--- TopPercentileFraud.sql
+++ TopPercentileFraud.sql
@@ -8,7 +8,6 @@ Your output should be policy number, state, claim cost, and fraud score.
 https://platform.stratascratch.com/coding/10303-top-percentile-fraud?code_type=5
 */
 
-
 ---SQL Server
 SELECT
   state,
@@ -29,3 +28,22 @@ WHERE
   fraud_score >= top_5_percentile
 ORDER BY
   state, fraud_score DESC;
+
+
+--Python
+import pandas as pd
+import numpy as np
+
+df = fraud_score
+
+# Calculate the 95th percentile for each state based on fraud_score
+df['top_5_percentile'] = df.groupby('state')['fraud_score'].transform(lambda x: np.percentile(x, 95))
+
+# Filter rows based on the top 5 percentile
+result = df[df['fraud_score'] >= df['top_5_percentile']]
+
+
+# Sort the result
+result = result.sort_values(by='state', ascending=True)
+
+return result[['policy_num', 'state', 'claim_cost', 'fraud_score']]
