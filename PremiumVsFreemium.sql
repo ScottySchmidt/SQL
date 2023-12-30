dbe901b7a33d2e26e0e368a9cbf98eaacabc0132
Scott Schmidt, MBA <scott.schmidt1989@yahoo.com>
2023-12-30 16:33:52 -0600

    Create PremiumVsFreemium.sql

---
+++ PremiumVsFreemium.sql
@@ -0,0 +1,30 @@
+/*
+Premium vs Freemium, Hard SQL: https://platform.stratascratch.com/coding/10300-premium-vs-freemium/submissions?code_type=3
+Find the total number of downloads for paying and non-paying users by date. 
+Include only records where non-paying customers have more downloads than paying customers. 
+The output should be sorted by earliest date first and contain 3 columns date, non-paying downloads, paying downloads.
+*/
+
+with cte as (SELECT d.date, d.downloads, 
+a.paying_customer
+FROM ms_user_dimension u
+INNER JOIN ms_acc_dimension a
+ON u.acc_id = a.acc_id
+INNER JOIN ms_download_facts d
+ON d.user_id = u.user_id
+ORDER BY d.date
+),
+
+day_metrics as(SELECT date, 
+sum(CASE WHEN paying_customer = 'yes' THEN downloads 
+END) as pay,
+sum(CASE WHEN paying_customer = 'no' THEN downloads 
+END) as free
+FROM cte
+GROUP BY date
+ORDER BY date
+)
+
+SELECT date, free, pay
+FROM day_metrics
+WHERE free > pay
