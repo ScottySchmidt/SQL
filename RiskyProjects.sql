601df21c38d3702d06b373159511851d18e58454
Scott Schmidt, MBA <scott.schmidt1989@yahoo.com>
2023-12-31 12:17:00 -0600

    Update RiskyProjects.sql

--- RiskyProjects.sql
+++ RiskyProjects.sql
@@ -1,59 +1,42 @@
 /*
 Risky Projects: https://platform.stratascratch.com/coding/10304-risky-projects?code_type=5
 
-Identify projects that are at risk for going overbudget. A project is considered to be overbudget if the cost of all employees assigned to the project is greater than the budget of the project.
-You'll need to prorate the cost of the employees to the duration of the project. For example, if the budget for a project that takes half a year to complete is $10K, then the total half-year salary of all employees assigned to the project should not exceed $10K.
+Identify projects that are at risk for going overbudget. 
+A project is considered to be overbudget if the cost of all employees assigned to the project is greater than the budget of the project.
+You'll need to prorate the cost of the employees to the duration of the project.
+For example, if the budget for a project that takes half a year to complete is $10K,
+then the total half-year salary of all employees assigned to the project should not exceed $10K.
 Salary is defined on a yearly basis, so be careful how to calculate salaries for the projects that last less or more than one year.
 Output a list of projects that are overbudget with their project name, project budget, and prorated total employee expense (rounded to the next dollar amount).
 HINT: to make it simpler, consider that all years have 365 days. You don't need to think about the leap years.
 */
 
--- MY_SQL
-WITH p AS (
-    SELECT REPLACE(title, 'Project', '') AS project_id, title, budget,
-           DATEDIFF(end_date, start_date) / 365 AS year_percent
-    FROM linkedin_projects
+-- SQL Server Solution:
+with budgets as(SELECT p.title, p.budget,
+datediff(day, p.start_date, p.end_date) as project_days, l.emp_id, e.salary
+FROM linkedin_projects p
+INNER JOIN linkedin_emp_projects l
+ON TRY_CAST(l.project_id AS INT) = p.id
+INNER JOIN linkedin_employees e
+ON l.emp_id = e.id
 ),
 
-cost as(SELECT p.project_id, sum(e.salary) as salary_cost
-FROM linkedin_employees e
-INNER JOIN linkedin_emp_projects p
-ON e.id = p.emp_id
-GROUP BY p.project_id
+salary as(SELECT title, sum(salary) as total_employee_expense
+FROM budgets
+GROUP BY title
 ),
 
-variance as(SELECT p.project_id, p.title, p.budget, p.year_percent, cost.salary_cost, 
-CEIL(p.year_percent*cost.salary_cost) as prorated_employee_expense
-FROM p
-INNER JOIN linkedin_emp_projects ep ON p.project_id = ep.project_id
-INNER JOIN cost ON cost.project_id = p.project_id
+variance as(SELECT b.title, b.budget,
+CEILING((CAST(s.total_employee_expense AS DECIMAL) * b.project_days) / 365.0) AS prorated_employee_expense
+FROM budgets b
+INNER JOIN salary s
+ON s.title = b.title
 )
 
-SELECT title, budget, prorated_employee_expense
+SELECT DISTINCT title, budget, prorated_employee_expense
 FROM variance
 WHERE prorated_employee_expense > budget
-;
-
---SQL Server (in process)
-WITH p AS (
-    SELECT REPLACE(title, 'Project', '') AS project_id, title, budget,
-           DATEDIFF(DAY, start_date, end_date) / 365.25 AS year_percent
-    FROM linkedin_projects
-),
-
-cost as(SELECT p.project_id, sum(e.salary) as salary_cost
-FROM linkedin_employees e
-INNER JOIN linkedin_emp_projects p
-ON e.id = p.emp_id
-GROUP BY p.project_id
-),
-
-variance as(SELECT p.project_id, p.title, p.budget, p.year_percent, cost.salary_cost, 
-CEILING(p.year_percent*cost.salary_cost) as prorated_employee_expense
-FROM p
-INNER JOIN linkedin_emp_projects ep ON p.project_id = ep.project_id
-INNER JOIN cost ON cost.project_id = p.project_id
-)
+ORDER BY title
 
 SELECT title, budget, prorated_employee_expense
 FROM variance
