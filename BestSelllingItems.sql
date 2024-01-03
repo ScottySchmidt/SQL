a18aedf3dacdd7600bd57d625c95df040232e035
Scott Schmidt, MBA <scott.schmidt1989@yahoo.com>
2024-01-03 15:46:32 -0600

    Update BestSelllingItems.sql

--- BestSelllingItems.sql
+++ BestSelllingItems.sql
@@ -1,11 +1,10 @@
-/* Best Selling Items, Hard SQL: https://platform.stratascratch.com/coding/10172-best-selling-item?code_type=3
+/* Best Selling Items, Hard Amazon and Ebay SQL: https://platform.stratascratch.com/coding/10172-best-selling-item?code_type=3
 Find the best selling item for each month (no need to separate months by year) where the biggest total invoice was paid. 
-The best selling item is calculated using the formula (unitprice * quantity). Output the month, the description of the item along with the amount paid.
+The best selling item is calculated using the formula (unitprice * quantity). 
+Output the month, the description of the item along with the amount paid.
 */
 
 -- YouTube Solution: https://www.youtube.com/watch?v=ON61CG7BvGs
-
--- My SQL Solution:
 with items as(SELECT month(invoicedate) as month, description, 
 sum(unitprice * quantity) as total_sales,
 dense_rank() OVER(PARTITION BY month(invoicedate) ORDER BY sum(unitprice * quantity) DESC) as ranked
