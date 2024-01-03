c49f1a5461d87e95976bbbd191bb0d6bb360f51c
Scott Schmidt, MBA <scott.schmidt1989@yahoo.com>
2024-01-03 11:39:33 -0600

    Update employeeBonus.sql

--- employeeBonus.sql
+++ employeeBonus.sql
@@ -5,16 +5,30 @@ Find the number of employees who received the bonus and who didn't. Bonus values
 Output value inside has_bonus column (1 if they had bonus, 0 if not) along with the corresponding number of employees for each.
 */
 
----SQL Server, MySQL, and Oracle all have same solution: The key to solving this problem is using a CASE statement.
-SELECT DISTINCT(e.id), e.first_name, e.last_name, 
-CASE 
-WHEN e.id IN ( 
-SELECT distinct(worker_ref_id) FROM bonus) 
-THEN 1
-ELSE 0 
-END as has_value 
+--SQL Server and MySQL Solution, Hard level but more so medium in my opinion:
+with emp_cte as( 
+SELECT DISTINCT(e.id),
+CASE
+WHEN e.id IN (SELECT worker_ref_id
+FROM bonus)
+THEN 1 ELSE 0
+END as has_value
 FROM employee e
-LEFT JOIN bonus b ON e.id = b.worker_ref_id
-ORDER BY has_value DESC;
+LEFT JOIN bonus b
+ON e.id = b.worker_ref_id
+)
 
---NOTE: ChatGPT incorrectly does the CASE statement like CASE WHEN b.worker_ref_id IS NOT NULL THEN 1 ELSE 0 END as has_bonus
+SELECT has_value, 
+count(has_value) as n_employees
+FROM emp_cte
+GROUP BY has_value
+ORDER BY has_value
+
+/* SQL OUTPUT:
+has_value	n_employees
+0	        27
+1	        3
+/*
+
+
+--NOTE: Can also do a case statement, ChatGPT incorrectly does the CASE statement like CASE WHEN b.worker_ref_id IS NOT NULL THEN 1 ELSE 0 END as has_bonus
