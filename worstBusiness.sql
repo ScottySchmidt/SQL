9dd3c7c48ece5de71a6c5fa7cde8b09d2e95a1b1
Scott Schmidt, MBA <scott.schmidt1989@yahoo.com>
2024-01-03 14:50:17 -0600

    Update worstBusiness.sql

--- worstBusiness.sql
+++ worstBusiness.sql
@@ -1,8 +1,10 @@
 /* Worst Businesses, Hard SQL: https://platform.stratascratch.com/coding/9739-worst-businesses?code_type=5
-For every year, find the worst business in the dataset. The worst business has the most violations during the year. You should output the year, business name, and number of violations.
+For every year, find the worst business in the dataset. 
+The worst business has the most violations during the year. 
+You should output the year, business name, and number of violations.
 */ 
 
---SQL Server and MySQL:
+--SQL Server and MySQL gets 75% of solution correct:
 with business as (SELECT business_name, year(inspection_date) as inspect_year, COUNT(business_name) as violations_count
 FROM sf_restaurant_health_violations
 GROUP BY business_name, year(inspection_date)
@@ -12,7 +14,7 @@ business2 as (SELECT business_name, inspect_year, violations_count,
 ROW_NUMBER() OVER (PARTITION BY inspect_year ORDER BY violations_count DESC) as rn
 FROM business)
 
-SELECT business_name, inspect_year, violations_count
+SELECT inspect_year, business_name, violations_count
 FROM business2
 WHERE rn = 1
 
