0a53653d9f9d6d685408eb81837347fc9c2eb60e
Scott Schmidt, MBA <scott.schmidt1989@yahoo.com>
2024-01-06 20:00:09 -0600

    Update CorrelationBetweenEmail&Activty.sql

--- CorrelationBetweenEmail&Activty.sql
+++ CorrelationBetweenEmail&Activty.sql
@@ -2,7 +2,8 @@
 Correlation Between E-mails And Activity Time - Hard Oracle SQL
 There are two tables with user activities. 
 The google_gmail_emails table contains information about emails being sent to users. 
-Each row in that table represents a message with a unique identifier in the id field. The google_fit_location table contains user activity logs from the Google Fit app.
+Each row in that table represents a message with a unique identifier in the id field. 
+The google_fit_location table contains user activity logs from the Google Fit app.
 Find the correlation between the number of emails received and the total exercise per day. 
 The total exercise per day is calculated by counting the number of user sessions per day.
 https://platform.stratascratch.com/coding/10069-correlation-between-e-mails-and-activity-time?code_type=7
