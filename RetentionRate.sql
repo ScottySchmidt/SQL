00ae5113c2c3fe5896107f3e8d6070173ecfd734
Scott Schmidt, MBA <scott.schmidt1989@yahoo.com>
2024-01-08 13:12:44 -0600

    Update RetentionRate.sql

--- RetentionRate.sql
+++ RetentionRate.sql
@@ -1,13 +1,15 @@
 /* 
+Retention Rate: Hard Meta/SakesForce SQL StrataSratch
 Find the monthly retention rate of users for each account separately for Dec 2020 and Jan 2021. 
 Retention rate is the percentage of active users an account retains over a given period of time.
 In this case, assume the user is retained if he/she stays with the app in any future months. 
 For example, if a user was active in Dec 2020 and has activity in any future month, consider them retained for Dec. 
 You can assume all accounts are present in Dec 2020 and Jan 2021. 
 Your output should have the account ID and the Jan 2021 retention rate divided by Dec 2020 retention rate.
+https://platform.stratascratch.com/coding/2053-retention-rate?code_type=3
 */
 
---- SQL Server Solution: 
+--- SQL Server Solution: A subquery is needed for this solution within each cte first.  
 with dec2020 as (SELECT account_id, 
 count(user_id) as count1
 FROM sf_events
