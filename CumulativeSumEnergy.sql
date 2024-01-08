0dca0826317ca0e24d7633e8d78ba458222398af
Scott Schmidt, MBA <scott.schmidt1989@yahoo.com>
2024-01-08 13:56:51 -0600

    Update CumulativeSumEnergy.sql

--- CumulativeSumEnergy.sql
+++ CumulativeSumEnergy.sql
@@ -1,5 +1,5 @@
 /*
-Cum Sum Energy Consumption, Hard: https://platform.stratascratch.com/coding/10084-cum-sum-energy-consumption?code_type=3
+Cum Sum Energy Consumption, Hard Meta Question: https://platform.stratascratch.com/coding/10084-cum-sum-energy-consumption?code_type=3
 Calculate the running total (i.e., cumulative sum) energy consumption of the Meta/Facebook data centers in all 3 continents by the date. 
 Output the date, running total energy consumption, and running total percentage rounded to the nearest whole number.
 */
