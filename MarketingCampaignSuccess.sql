be94e2abe662b8e455e7e5ce0e2520933eb0cdf6
Scott Schmidt, MBA <scott.schmidt1989@yahoo.com>
2024-01-09 19:32:35 -0600

    Update MarketingCampaignSuccess.sql

--- MarketingCampaignSuccess.sql
+++ MarketingCampaignSuccess.sql
@@ -1,8 +1,8 @@
-/* Marketing Campaign Success [Advanced] https://platform.stratascratch.com/coding/514-marketing-campaign-success-advanced?code_type=5
-  
+/* Marketing Campaign Success [Advanced]; Hard Amazon SQL 
 You have a table of in-app purchases by user. Users that make their first in-app purchase are placed in a marketing campaign where they see call-to-actions for more in-app purchases.
 Find the number of users that made additional in-app purchases due to the success of the marketing campaign.
 The marketing campaign doesn't start until one day after the initial in-app purchase so users that only made one or multiple purchases on the first day do not count, nor do we count users that over time purchase only the products they purchased on the first day.
+https://platform.stratascratch.com/coding/514-marketing-campaign-success-advanced?code_type=5
 */
 
 --- YouTube Solution: https://www.youtube.com/watch?v=azxXnO81OwQ
