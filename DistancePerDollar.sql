940987c909eff7dcd2a0f4d5137b6b5688162edb
Scott Schmidt, MBA <scott.schmidt1989@yahoo.com>
2024-01-11 14:47:27 -0600

    Update DistancePerDollar.sql

--- DistancePerDollar.sql
+++ DistancePerDollar.sql
@@ -3,13 +3,32 @@ Distance Per Dollar, Hard SQL Uber: https://platform.stratascratch.com/coding/10
 
 You’re given a dataset of uber rides with the traveling distance (‘distance_to_travel’) and cost (‘monetary_cost’) for each ride. 
 First, find the difference between the distance-per-dollar for each date and the average distance-per-dollar for that year-month. 
-Distance-per-dollar is defined as the distance traveled divided by the cost of the ride. Use the calculated difference on each date to calculate absolute average difference in distance-per-dollar metric on monthly basis (year-month).
---The output should include the year-month (YYYY-MM) and the absolute average difference in distance-per-dollar (Absolute value to be rounded to the 2nd decimal).
-You should also count both success and failed request_status as the distance and cost values are populated for all ride requests. Also, assume that all dates are unique in the dataset. Order your results by earliest request date first.
+Distance-per-dollar is defined as the distance traveled divided by the cost of the ride. 
+Use the calculated difference on each date to calculate absolute average difference in distance-per-dollar metric on monthly basis (year-month).
+The output should include the year-month (YYYY-MM) and the absolute average difference in distance-per-dollar (Absolute value to be rounded to the 2nd decimal).
+You should also count both success and failed request_status as the distance and cost values are populated for all ride requests. 
+Also, assume that all dates are unique in the dataset. Order your results by earliest request date first.
 */
 
--- SQL Server Solution: 
--- Create request_date to a year-month (YYYY-MM):
+-- Solution without a window function, easy to understand: 
+with daily as (SELECT FORMAT(CONVERT(DATETIME, request_date), 'yyyy-MM') AS year_month,
+round(distance_to_travel/monetary_cost, 2) as dist_per_dollar
+FROM uber_request_logs
+),
+
+avg_month as(SELECT year_month, avg(dist_per_dollar) as avg_month
+FROM daily
+GROUP BY year_month
+)
+
+SELECT d.year_month, avg(abs(d.dist_per_dollar-m.avg_month)) as difference
+FROM daily d
+INNER JOIN avg_month m 
+ON d.year_month = m.year_month
+GROUP BY d.year_month
+
+    
+-- SQL Server Solution: Create request_date to a year-month (YYYY-MM):
 with uber as(SELECT request_date, 
     FORMAT(CONVERT(DATETIME, request_date), 'yyyy-MM') AS year_month,
     ROUND(distance_to_travel / monetary_cost, 2) AS distance_per_dollar
