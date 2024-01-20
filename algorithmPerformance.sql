cf6ec6569c6e9cf1c3b478d5561ca09754d25e8b
Scott Schmidt, MBA <scott.schmidt1989@yahoo.com>
2024-01-20 12:30:05 -0600

    Update AlgorithmPerformance.sql

--- algorithmPerformance.sql
+++ algorithmPerformance.sql
@@ -1,6 +1,9 @@
 /* 
-Algorithm Performance: https://platform.stratascratch.com/coding/10350-algorithm-performance?code_type=1: Meta/Facebook is developing a search algorithm that will allow users to search through their post history. You have been assigned to evaluate the performance of this algorithm.
+YouTube Solution: https://www.youtube.com/watch?v=lBuUzWqP61Y
+Algorithm Performance, Hard SQL
 
+Meta/Facebook is developing a search algorithm that will allow users to search through their post history. 
+You have been assigned to evaluate the performance of this algorithm.
 We have a table with the user's search term, search result positions, and whether or not the user clicked on the search result.
 
 Write a query that assigns ratings to the searches in the following way:
@@ -8,7 +11,8 @@ Write a query that assigns ratings to the searches in the following way:
 •	If the search was clicked but the top position of clicked terms was outside the top 3 positions, assign the search a rating=2
 •	If the search was clicked and the top position of a clicked term was in the top 3 positions, assign the search a rating=3
 
-As a search ID can contain more than one search term, select the highest rating for that search ID. Output the search ID and its highest rating.
+As a search ID can contain more than one search term, select the highest rating for that search ID. 
+Output the search ID and its highest rating.
 
 Example: The search_id 1 was clicked (clicked = 1) and its position is outside of the top 3 positions (search_results_position = 5), therefore its rating is 2.
 */
